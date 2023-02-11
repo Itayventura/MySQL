8e9e94422c4e3caa13fceff5d54bc9849a714995
itayventura <ventura.itay@gmail.com>
2023-02-11 19:44:09 +0200

    SELECT less byttes

--- queries/select_fewer_columns.sql
+++ queries/select_fewer_columns.sql
@@ -5,3 +5,7 @@ from users
 --select only phone number - if you just want to send sms to all users.
 select phone_number
 from users
+
+--select less bytes is also faster
+select integer_column
+from users
